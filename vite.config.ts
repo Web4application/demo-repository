8a585d176eb79144704932ea7f35fb61d9925dc1
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 19:07:33 +0200

    Update vite.config.ts
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: './postcss.config.js'
-  }
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Demo Repo App',
+        short_name: 'DemoApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#42b883',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
